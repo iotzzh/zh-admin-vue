eafb51fa5fdbde13fc6f01021e7bba56cdb8a5d1
zzh <956159241@qq.com>
2022-09-15 23:42:35 +0800

    添加modal组件

---
+++ src/components/zh-modal/Index.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-dialog
+    v-model="modal.show"
+    :title="modal.title"
+    :closeOnClickModal="false"
+    :width="modal.width"
+    @close="zhModal.close"
+    append-to-body
+    :top="modal.top"
+    :fullscreen="modal.fullscreen"
+  >
+  <slot></slot>
+
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="zhModal.cancel">取消</el-button>
+        <el-button type="primary" @click="zhModal.submit">确定</el-button>
+      </span>
+      <slot name="footer" />
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { toRefs, PropType } from 'vue';
+import { ZHModal } from './index';
+import { TModal } from './type';
+
+const props = defineProps({
+  modal: {
+    type: Object as PropType<TModal>,
+    required: true, // 必传
+  },
+});
+
+const { modal } = toRefs(props);
+
+const emit = defineEmits(['close', 'submit', 'cancel' ]);
+
+const zhModal = new ZHModal({ modal, emit });
+</script>
+
+<script lang="ts">
+export default { name: 'ZHModal' };
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+<style lang="scss">
+.el-dialog__header {
+  text-align: center;
+  font-size: 18px;
+  font-weight: bolder;
+  border-bottom: 1px solid rgba($color: #000000, $alpha: 0.1);
+}
+</style>
+
+
