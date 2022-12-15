c9422edce0c61b7e667dd18bd4069f98b65c5285
张中华 <956159241@qq.com>
2022-12-15 14:06:07 +0800

    save code temp.

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,16 +1,3 @@
-/*
- * @Author: zzh
- * @Date: 2022-02-28 14:52:53
- * @LastEditors: zzh
- * @LastEditTime: 2022-11-23 14:46:38
- * @Description: 同一导出store， 参考 redux， reduce.compine()
- * @FilePath: \zh-admin\src\stores\index.ts
- */
-
-// import { useLayoutStore } from './layout';
-
-// export { useLayoutStore };
-
 import type { App } from 'vue';
 import { createPinia } from 'pinia';
 
