6066ffe981da98bc738acd985981ea34312251e0
张中华 <956159241@qq.com>
2023-05-17 16:34:57 +0800

    添加正方形组件

---
+++ src/components/square.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dot" style="display: inline-block;" :style="{width: sideLength, height: sideLength, backgroundColor: color }" ></div>
+</template>
+<script lang="ts" setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+    sideLength: {
+        type: String
+    },
+    color: {
+        type: String
+    }
+});
+
+const { sideLength, color } = toRefs(props);
+</script>
+
+<script lang="ts">
+export default { name: 'square' };
+</script>
+
+
