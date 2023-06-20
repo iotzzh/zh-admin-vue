0f7434475d63c825b212613e9e992d93aa79af2a
zhangzhonghua <956159241@qq.com>
2023-06-20 16:44:36 +0800

    添加一个高内聚的弹窗

---
+++ src/components/zh-modal2/index.ts
@@ -0,0 +1,42 @@
+import { Ref, ref } from 'vue';
+import { TZHModal } from './type';
+
+export class ZHModal {
+    emit: (event: 'closed' | 'submited' | 'canceled' | 'opened', ...args: any[]) => void;
+    modal: Ref<TZHModal>;
+    fullscreen: Ref<boolean>;
+    constructor(modal: Ref<TZHModal>, emit: (event: 'closed' | 'submited' | 'canceled' | 'opened', ...args: any[]) => void) {
+        this.modal = modal;
+        this.emit = emit;
+        this.fullscreen = ref(modal.value.fullscreen || false);
+    }
+
+    close = () => {
+        this.modal.value.show = false;
+        this.emit('closed');
+    };
+
+    private executeOnBeforeSubmit = () => {
+        if (!this.modal.value.onBeforeSubmit) return;
+        if (typeof this.modal.value.onBeforeSubmit === 'string')
+            (new Function('params', this.modal.value.onBeforeSubmit))(this.modal.value);
+        else
+            this.modal.value.onBeforeSubmit(this.modal.value);
+    };
+
+    submit = async () => {
+        this.executeOnBeforeSubmit();
+        this.modal.value.show = false;
+        this.emit('submited');
+    };
+
+    cancel = () => {
+        this.modal.value.show = false;
+        this.emit('canceled');
+    };
+
+    toggleFullScreen = () => {
+        this.fullscreen.value = !this.fullscreen.value;
+    };
+
+}
---
+++ src/components/zh-modal2/readme.md
@@ -0,0 +1,17 @@
+# 表单
+## 配置：
+见type.ts
+
+## 事件：
+### @close = () => {};
+### @submit = () => {};
+### @cancel = () => {};
+
+## 方法：
+### onBeforeSubmit(modal): void
+### onAfterSubmit(modal): void
+
+
+## 依赖
+### @/utils/storage
+```storage.getIsMobile()```判断是否是手机端
