6248f61d55c24ed4ddfb0ffa91f627c0dea743c2
zhangzhonghua <956159241@qq.com>
2023-06-21 14:59:27 +0800

    优化首页样式

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="box">
     <el-row class="row list" :gutter="20">
-      <el-col :span="6"><div class="column">
+      <el-col :xl="6" :lg="6" :md="6" :sm="12" :xs="24"><div class="column">
         <img class="list-img" :src="Img1" alt="img" />
         <div class="text">今日订单数量</div>
         <div class="num">3779</div>
         <div class="tag"><span>-47%</span>同比昨日新增</div>
       </div></el-col>
-      <el-col :span="6"><div class="column">
+      <el-col :xl="6" :lg="6" :md="6" :sm="12" :xs="24"><div class="column">
         <img class="list-img" :src="Img2" alt="img" />
         <div class="text">今日新增诊所</div>
         <div class="num">3</div>
         <div class="tag"><span>-70%</span>同比昨日新增</div>
       </div></el-col>
-      <el-col :span="6"><div class="column">
+      <el-col :xl="6" :lg="6" :md="6" :sm="12" :xs="24"><div class="column">
         <img class="list-img" :src="Img3" alt="img" />
         <div class="text">今日就诊人数</div>
         <div class="num">3779</div>
         <div class="tag"><span>-45%</span>同比昨日新增</div>
       </div></el-col>
-      <el-col :span="6"><div class="column">
+      <el-col :xl="6" :lg="6" :md="6" :sm="12" :xs="24"><div class="column">
         <img class="list-img" :src="Img4" alt="img" />
         <div class="text">今日退单数量</div>
         <div class="num" style="color: red;">56</div>
@@ -28,19 +28,19 @@
     </el-row>
 
     <el-row class="row" :gutter="20">
-      <el-col :span="12"><div class="column">
+      <el-col :md="12" :sm="12" :xs="24"><div class="column">
         <div v-loading="loading1" class="chart chart1"></div>
       </div></el-col>
-      <el-col :span="12"><div class="column">
+      <el-col :md="12" :sm="12" :xs="24"><div class="column">
         <div v-loading="loading2" class="chart chart2"></div>
       </div></el-col>
     </el-row>
 
     <el-row class="row" :gutter="20">
-      <el-col :span="12"><div class="column">
+      <el-col :md="12" :sm="12" :xs="24"><div class="column">
         <div v-loading="loading3" class="chart chart3"></div>
       </div></el-col>
-      <el-col :span="12"><div class="column">
+      <el-col :md="12" :sm="12" :xs="24"><div class="column">
         <div v-loading="loading4" class="chart chart4"></div>
       </div></el-col>
     </el-row>
