be87f4192393b34f2adc167cafbbaa588d002f12
zhangzhonghua <956159241@qq.com>
2023-07-26 10:40:57 +0800

    删除无用示例

--- src/examples/api/tree/index.ts
+++
@@ -1,8 +0,0 @@
-import { apiMock } from '@/api/config';
-
-export default {
-    getOrgList: apiMock + '/org/list',
-    addOrg: apiMock + '/org/add',
-    updateOrg: apiMock + '/org/update',
-    deleteOrg: apiMock + '/org/delete',
-};
