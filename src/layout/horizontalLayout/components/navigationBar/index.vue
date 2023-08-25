52c4719c649acd1baa21c0394debdfa53ff594c3
zhangzhonghua <956159241@qq.com>
2023-08-25 11:25:58 +0800

    添加登录界面

--- src/layout/horizontalLayout/components/navigationBar/index.vue
+++ src/layout/horizontalLayout/components/navigationBar/index.vue
@@ -17,10 +17,9 @@
     <!-- info -->
     <div class="info">
       <!-- <span>搜索</span> -->
-      <span class="bell"
-        ><el-badge is-dot class="item"
-          ><el-icon> <Bell /> </el-icon></el-badge
-      ></span>
+      <span class="bell"><el-badge is-dot class="item"><el-icon>
+            <Bell />
+          </el-icon></el-badge></span>
 
       <span class="fullscreen" @click="toggleFullScreen">
         <i v-if="fullscreen" class="iconfont icon-fullscreen-shrink"></i>
@@ -52,266 +51,269 @@
 </template>
 
 <script setup lang="ts">
-  import SidebarLogo from './SidebarLogo.vue';
-  import SidebarItem from './SidebarItem.vue';
-  import { ref, reactive, onMounted } from 'vue';
-  import { useLayoutStore } from '@/layout/store';
-  import UIHelper from '@/utils/uiHelper';
-  import { appendRouter, router } from '@/router';
-  import { RouteRecordRaw } from 'vue-router';
-  import ZHRequest from '@/components/zh-request';
-  import storage from '@/utils/storage';
-  import api from '@/api';
-  import { useLocale } from '@/locales/useLocale';
-  import { LocaleType } from '@/locales/type';
+import SidebarLogo from './SidebarLogo.vue';
+import SidebarItem from './SidebarItem.vue';
+import { ref, reactive, onMounted } from 'vue';
+import { useLayoutStore } from '@/layout/store';
+import UIHelper from '@/utils/uiHelper';
+import { appendRouter, router } from '@/router';
+import { RouteRecordRaw } from 'vue-router';
+import ZHRequest from '@/components/zh-request';
+import storage from '@/utils/storage';
+import api from '@/api';
+import { useLocale } from '@/locales/useLocale';
+import { LocaleType } from '@/locales/type';
 
-  const ROUTE_DATA_SOURCE = import.meta.env.VITE_ROUTE_DATA_SOURCE || 'file';
+const ROUTE_DATA_SOURCE = import.meta.env.VITE_ROUTE_DATA_SOURCE || 'file';
 
-  const store = useLayoutStore();
+const store = useLayoutStore();
 
-  const menuList = ref([] as any);
-  const userInfo = ref({} as any);
-  onMounted(() => {
-    // userInfo.value = storage?.getUserInfo();
-    userInfo.value = {
-      name: '测试名',
-    };
-  });
-
-  onMounted(async () => {
-    const params = {
-      url: api.getMenuList,
-      conditions: {},
-    };
-    const result = await ZHRequest.post(params);
-    menuList.value = result.data.records;
-
-    if (ROUTE_DATA_SOURCE === 'api') appendRouter(result.data.records);
-  });
-
-  // 退出登录事件
-  const handleCommand = (command: string | number | object) => {
-    if (command === 'logout') {
-      sessionStorage.clear();
-      localStorage.clear();
-      router && router.push('/');
-      location.reload();
-    }
+const menuList = ref([] as any);
+const userInfo = ref({} as any);
+onMounted(() => {
+  // userInfo.value = storage?.getUserInfo();
+  userInfo.value = {
+    name: '测试名',
   };
+});
 
-  const fullscreen = ref(false);
-  const toggleFullScreen = () => {
-    UIHelper.toggleFullScreen(document.body, !fullscreen.value);
-    fullscreen.value = !fullscreen.value;
+onMounted(async () => {
+  const params = {
+    url: api.getMenuList,
+    conditions: {},
   };
+  const result = await ZHRequest.post(params);
+  menuList.value = result?.data?.records || [];
 
-  const locale = useLocale();
-  const changeLanguage = async (command: string | number | object) => {
-    await locale.changeLocale(command as LocaleType);
-  };
+  if (ROUTE_DATA_SOURCE === 'api') appendRouter(result.data.records);
+});
 
-  const clickChangeLayout = () => store.setLayout('vertical');
+// 退出登录事件
+const handleCommand = (command: string | number | object) => {
+  if (command === 'logout') {
+    sessionStorage.clear();
+    localStorage.clear();
+    router && router.push('/');
+    location.reload();
+  }
+};
+
+const fullscreen = ref(false);
+const toggleFullScreen = () => {
+  UIHelper.toggleFullScreen(document.body, !fullscreen.value);
+  fullscreen.value = !fullscreen.value;
+};
+
+const locale = useLocale();
+const changeLanguage = async (command: string | number | object) => {
+  await locale.changeLocale(command as LocaleType);
+};
+
+const clickChangeLayout = () => store.setLayout('vertical');
 </script>
 
 <style lang="scss" scoped>
-  .nav-bar {
-    background-color: white;
-    height: 60px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    box-sizing: border-box;
-    padding: 0px;
+.nav-bar {
+  background-color: white;
+  height: 60px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 0px;
 
-    .logo {
-      width: 200px;
-      height: 100%;
-    }
+  .logo {
+    width: 200px;
+    height: 100%;
+  }
 
-    .nav {
-      flex: 1;
-      overflow: auto;
+  .nav {
+    flex: 1;
+    overflow: auto;
 
-      .scrollbar {
-        text-align: right;
-        background-color: #f5f5f5;
-
-        .menu {
-          background-color: $mainColor;
-          border-bottom: none;
-          height: 100%;
-          justify-content: right;
-        }
-      }
-    }
-
-    .info {
-      line-height: 59px;
-      height: 59px;
+    .scrollbar {
       text-align: right;
-      vertical-align: middle;
+      background-color: #f5f5f5;
 
-      .el-dropdown {
-        vertical-align: middle !important;
-        cursor: pointer;
-        font-size: 14px;
-        color: $menu-text-color;
-        font-weight: bolder;
+      .menu {
+        background-color: $mainColor;
+        border-bottom: none;
+        height: 100%;
+        justify-content: right;
       }
     }
-
-    .fullScreen {
-      line-height: 59px;
-      margin-left: 15px;
-      vertical-align: middle;
-      cursor: pointer;
-    }
-
-    span,
-    .lang {
-      padding-left: 12px !important;
-    }
-
-    .name {
-      height: 18px;
-      font-weight: bolder;
-      font-size: 17px !important;
-    }
-
-    .lang {
-      font-size: 20px !important;
-    }
-
-    .fullscreen {
-      cursor: pointer;
-      position: relative;
-      top: 3px;
-      .iconfont {
-        font-size: 20px !important;
-      }
-    }
-
-    .el-dropdown {
-      vertical-align: middle !important;
-      cursor: pointer;
-      font-size: 14px;
-      font-weight: bolder;
-    }
-
-    .setting-icon {
-      cursor: pointer;
-    }
-
-    .bell {
-      cursor: pointer;
-      .item {
-        height: 21px;
-        position: relative;
-        &:deep(.el-icon) {
-          font-size: 20px !important;
-          position: absolute;
-          right: 0px;
-          top: 0px;
-        }
-      }
-    }
-  }
-
-  .nav-bar .info .el-dropdown {
-    color: var(--el-menu-text-color);
-  }
-
-  .el-menu.el-menu--horizontal.menu {
-    // background-color: rgb(12,33,53) !important;
-    background-color: white !important;
-    --el-menu-text-color: black !important;
-    --el-menu-hover-text-color: var(--el-color-primary) !important;
-    --el-menu-bg-color: rgb(12, 33, 53);
-    // --el-menu-hover-bg-color: rgba(9,96,189, 0.8) !important;
-    // --el-menu-hover-bg-color: white !important;
-    // --el-color-primary
-    // --el-menu-active-color: rgb(255, 255, 255, 1) !important;
-    --el-menu-active-color: var(--el-color-primary) !important;
-
-    &:deep(.el-menu-item) {
-      // color: rgb(255, 255, 255, 0.8) !important;
-      color: black !important;
-    }
-
-    &:deep(.el-menu-item.is-active) {
-      // color: white !important;
-      // background-color: rgba(9,96,189, 0.8);
-      // background-color: var(--el-color-primary);
-      // background-color: white !important;
-    }
-  }
-
-  .el-menu--horizontal > .el-menu-item.is-active {
-    border-bottom: 2px solid rgba(9, 96, 189, 1) !important;
-  }
-
-  .el-sub-menu__title:hover {
-    // background-color: red !important;
-  }
-
-  .el-menu--horizontal > .el-sub-menu .el-sub-menu__title:hover {
-    // background-color: red;
-  }
-
-  .el-menu--horizontal > .el-menu-item:not(.is-disabled):focus {
-    background-color: transparent !important;
-    color: black !important;
   }
 
   .info {
-    width: 250px;
     line-height: 59px;
     height: 59px;
     text-align: right;
     vertical-align: middle;
-    padding-right: 22px;
-
-    .bell {
-      cursor: pointer;
-
-      .item {
-        height: 21px;
-        position: relative;
-
-        &:deep(.el-icon) {
-          position: absolute;
-          right: 0px;
-          top: 0px;
-        }
-      }
-    }
-
-    span {
-      padding: 0px 5px;
-    }
-
-    .name {
-      height: 18px;
-    }
-
-    .fullscreen {
-      cursor: pointer;
-    }
 
     .el-dropdown {
       vertical-align: middle !important;
       cursor: pointer;
       font-size: 14px;
+      color: $menu-text-color;
       font-weight: bolder;
     }
+  }
 
-    .setting-icon {
-      cursor: pointer;
+  .fullScreen {
+    line-height: 59px;
+    margin-left: 15px;
+    vertical-align: middle;
+    cursor: pointer;
+  }
+
+  span,
+  .lang {
+    padding-left: 12px !important;
+  }
+
+  .name {
+    height: 18px;
+    font-weight: bolder;
+    font-size: 17px !important;
+  }
+
+  .lang {
+    font-size: 20px !important;
+  }
+
+  .fullscreen {
+    cursor: pointer;
+    position: relative;
+    top: 3px;
+
+    .iconfont {
+      font-size: 20px !important;
     }
   }
 
-  .iconfont {
-    margin-right: 0px;
-    padding-right: 0px;
+  .el-dropdown {
+    vertical-align: middle !important;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: bolder;
   }
+
+  .setting-icon {
+    cursor: pointer;
+  }
+
+  .bell {
+    cursor: pointer;
+
+    .item {
+      height: 21px;
+      position: relative;
+
+      &:deep(.el-icon) {
+        font-size: 20px !important;
+        position: absolute;
+        right: 0px;
+        top: 0px;
+      }
+    }
+  }
+}
+
+.nav-bar .info .el-dropdown {
+  color: var(--el-menu-text-color);
+}
+
+.el-menu.el-menu--horizontal.menu {
+  // background-color: rgb(12,33,53) !important;
+  background-color: white !important;
+  --el-menu-text-color: black !important;
+  --el-menu-hover-text-color: var(--el-color-primary) !important;
+  --el-menu-bg-color: rgb(12, 33, 53);
+  // --el-menu-hover-bg-color: rgba(9,96,189, 0.8) !important;
+  // --el-menu-hover-bg-color: white !important;
+  // --el-color-primary
+  // --el-menu-active-color: rgb(255, 255, 255, 1) !important;
+  --el-menu-active-color: var(--el-color-primary) !important;
+
+  &:deep(.el-menu-item) {
+    // color: rgb(255, 255, 255, 0.8) !important;
+    color: black !important;
+  }
+
+  &:deep(.el-menu-item.is-active) {
+    // color: white !important;
+    // background-color: rgba(9,96,189, 0.8);
+    // background-color: var(--el-color-primary);
+    // background-color: white !important;
+  }
+}
+
+.el-menu--horizontal>.el-menu-item.is-active {
+  border-bottom: 2px solid rgba(9, 96, 189, 1) !important;
+}
+
+.el-sub-menu__title:hover {
+  // background-color: red !important;
+}
+
+.el-menu--horizontal>.el-sub-menu .el-sub-menu__title:hover {
+  // background-color: red;
+}
+
+.el-menu--horizontal>.el-menu-item:not(.is-disabled):focus {
+  background-color: transparent !important;
+  color: black !important;
+}
+
+.info {
+  width: 250px;
+  line-height: 59px;
+  height: 59px;
+  text-align: right;
+  vertical-align: middle;
+  padding-right: 22px;
+
+  .bell {
+    cursor: pointer;
+
+    .item {
+      height: 21px;
+      position: relative;
+
+      &:deep(.el-icon) {
+        position: absolute;
+        right: 0px;
+        top: 0px;
+      }
+    }
+  }
+
+  span {
+    padding: 0px 5px;
+  }
+
+  .name {
+    height: 18px;
+  }
+
+  .fullscreen {
+    cursor: pointer;
+  }
+
+  .el-dropdown {
+    vertical-align: middle !important;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: bolder;
+  }
+
+  .setting-icon {
+    cursor: pointer;
+  }
+}
+
+.iconfont {
+  margin-right: 0px;
+  padding-right: 0px;
+}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,77 +1,115 @@
 <template>
-  <div class="main-content">
-    <div class="login-box" v-on:keypress.enter="login" v-if="showLoginForm">
-      <div class="user-box">
-        <div class="login-title">{{ sysName }}</div>
-        <form class="login-form" v-if="showLoginForm">
-          <el-input
-            v-model="inputAccount"
-            class="w-50 m-2 input-box"
-            size="large"
-            placeholder="随便输入"
-            :prefix-icon="Avatar"
-            clearable
-          ></el-input>
-          <el-input
-            v-model="inputPassword"
-            class="w-50 m-2 input-box"
-            size="large"
-            placeholder="随便输入"
-            type="password"
-            :prefix-icon="Lock"
-            clearable
-            show-password
-          ></el-input>
-          <el-button type="primary" class="login-button button" @click="login()">登录</el-button>
+  <div class="box" v-loading="loading">
+    <div class="container right-panel-active">
+      <!-- Sign Up -->
+      <div class="container__form container--signup">
+        <form action="#" class="form" id="form1">
+          <h2 class="form__title">登录</h2>
+          <input type="text" placeholder="用户名（任意输入）" class="input" />
+          <input type="text" placeholder="邮箱（任意输入）" class="input" />
+          <input type="password" placeholder="密码（任意输入）" class="input" />
+          <button class="btn">登录</button>
         </form>
+      </div>
+
+      <!-- Sign In -->
+      <div class="container__form container--signin">
+        <form action="#" class="form" id="form2">
+          <h2 class="form__title">注册</h2>
+          <input type="email" placeholder="Email" class="input" />
+          <input type="password" placeholder="Password" class="input" />
+          <a href="#" class="link">忘记密码?</a>
+          <button class="btn">注册</button>
+        </form>
+      </div>
+
+      <!-- Overlay -->
+      <div class="container__overlay">
+        <div class="overlay">
+          <div class="overlay__panel overlay--left">
+            <button class="btn" id="signIn">注册</button>
+          </div>
+          <div class="overlay__panel overlay--right">
+            <button class="btn" id="signUp">登录</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import { Avatar, Lock } from '@element-plus/icons-vue';
-  import { useRouter } from 'vue-router';
-  import storage from '@/utils/storage';
-  import { TZHRequestParams } from '@/components/zh-request/type';
-  import ZHRequest from '@/components/zh-request';
-  import api from '@/api';
+import { ref, onMounted } from 'vue';
+import { Avatar, Lock } from '@element-plus/icons-vue';
+import { useRouter } from 'vue-router';
+import storage from '@/utils/storage';
+import { TZHRequestParams } from '@/components/zh-request/type';
+import ZHRequest from '@/components/zh-request';
+import api from '@/api';
 
-  const router = useRouter();
+const router = useRouter();
+const loading = ref(false);
 
-  const sysName = ref('后台管理系统');
-  const inputAccount = ref('') as any;
-  const inputPassword = ref('') as any;
-  const showLoginForm = ref(true);
+const sysName = ref('后台管理系统');
+const inputAccount = ref('') as any;
+const inputPassword = ref('') as any;
+const showLoginForm = ref(true);
 
-  const setToken = (token: string) => storage.setToken(token);
+const setToken = (token: string) => storage.setToken(token);
 
-  // 登录
-  const login = async () => {
-    const params: TZHRequestParams = {
-      url: api.login,
-      conditions: {
-        loginId: inputAccount.value,
-        loginPass: inputPassword.value,
-      },
-      errorMessage: '登录失败',
-      successMessage: '登录成功',
-    };
-
-    const result = await ZHRequest.get(params);
-    if (!result.success) return;
-    setToken(result.data.token);
-
-    // 跳转路由
-    router.push({ path: '/root' });
+// 注册
+const login = async () => {
+  loading.value = true;
+  const params: TZHRequestParams = {
+    url: api.login,
+    conditions: {
+      loginId: inputAccount.value,
+      loginPass: inputPassword.value,
+    },
+    errorMessage: '登录失败',
+    successMessage: '登录成功',
   };
+
+  const result = await ZHRequest.get(params);
+  loading.value = false;
+  if (!result.success) return;
+  setToken(result.data.token);
+
+  // 跳转路由
+  router.push({ path: '/root' });
+};
+
+
+onMounted(() => {
+  const signInBtn: any = document.getElementById('signIn');
+  const signUpBtn: any = document.getElementById('signUp');
+  const fistForm: any = document.getElementById('form1');
+  const secondForm: any = document.getElementById('form2');
+  const container: any = document.querySelector('.container');
+
+  signInBtn.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+  });
+
+  signUpBtn.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+  });
+
+  fistForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    login();
+  });
+  secondForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    login();
+  });
+});
 </script>
 
 <script lang="ts">
-  export default { name: 'login' };
+export default { name: 'login' };
 </script>
 
 <style lang="scss" scoped>
-  @import './index.scss';
+@import './index.scss';
 </style>
