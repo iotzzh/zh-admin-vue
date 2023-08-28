cd67826d04c9cf973447b2802bc1487aa37d3426
zhangzhonghua <956159241@qq.com>
2023-08-28 16:08:06 +0800

    添加一点关于请求替换的想法

--- src/templatePage/basic/index.vue
+++ src/templatePage/basic/index.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script lang="ts" setup>
-  import BasicTemplate from '@/templates/basic/index.vue';
-  import { ref } from 'vue';
-  import settings from './index.json';
+import BasicTemplate from '@/templates/basic/index.vue';
+import { ref } from 'vue';
+import settings from './index.json';
+import api from '@/api';
 
-  const pageSetting: any = ref(settings);
+const pageSetting: any = ref(settings);
+
+const convertRequest = () => {
+  if (pageSetting.value?.requestConfig?.urlList) pageSetting.value.requestConfig.urlList = api[pageSetting.value.requestConfig.urlList];
+};
+convertRequest();
 </script>
---
+++ src/views/test/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <BasicTemplate :config="pageSetting"></BasicTemplate>
+</template>
+
+<script lang="ts" setup>
+  import BasicTemplate from '@/templates/basic/index.vue';
+  import { ref } from 'vue';
+  import settings from './index.json';
+
+  const pageSetting: any = ref(settings);
+</script>
