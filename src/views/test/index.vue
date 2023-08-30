5cce4a9e048bf8f6d53a73a4cdc722c5cf86eee9
zhangzhonghua <956159241@qq.com>
2023-08-30 16:35:03 +0800

    优化模板

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -6,6 +6,13 @@
   import BasicTemplate from '@/templates/basic/index.vue';
   import { ref } from 'vue';
   import settings from './index.json';
+  import api from '@/api';
 
   const pageSetting: any = ref(settings);
+
+  const convertRequest = () => {
+    if (settings.table.requestConfig?.list?.url)
+      pageSetting.value.table.requestConfig.list.url = api[settings.table.requestConfig?.list?.url];
+  };
+  convertRequest();
 </script>
