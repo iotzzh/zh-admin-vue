6f881e57a4a6064f75c622c27f8e8973354c9d70
zhangzhonghua <956159241@qq.com>
2023-10-08 13:53:07 +0800

    添加一个显示组件的模块

---
+++ src/views/component/form.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        form-item
+    </div>    
+</template>
+
+<script lang="ts" setup>
+import ZHForm from '@/components/zh-form/index.vue';
+import ZHBox from '@/components/zh-box/index.vue';
+</script>
