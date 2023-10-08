bab99c8e5c632815ea006afb0f5832704db4d943
zhangzhonghua <956159241@qq.com>
2023-10-08 15:51:17 +0800

    暂存，去添加一个配置面板

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ import 'splitpanes/dist/splitpanes.css';
 import { createApp } from 'vue';
 import { setupStore } from '@/stores/index';
 
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+
 import Particles from "vue3-particles";
 import { setupRouter } from '@/router/index';
 import ElementPlus from 'element-plus';
@@ -61,6 +68,27 @@ async function boostrap() {
   // app.config.globalProperties.$utils = utils;
   globalThis.$utils = utils;
   app.mount('#app');
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: worker
+  self.MonacoEnvironment = {
+    getWorker(_: string, label: string) {
+      if (label === 'json') {
+        return new jsonWorker();
+      }
+      if (['css', 'scss', 'less'].includes(label)) {
+        return new cssWorker();
+      }
+      if (['html', 'handlebars', 'razor'].includes(label)) {
+        return new htmlWorker();
+      }
+      if (['typescript', 'javascript'].includes(label)) {
+        return new tsWorker();
+      }
+      return new EditorWorker();
+    }
+  };
+
 }
 
 boostrap();
