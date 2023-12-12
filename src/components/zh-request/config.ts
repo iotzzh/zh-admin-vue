18763ea4ad635e27b058a272823ca540572da693
zzh <956159241@qq.com>
2023-12-12 22:57:39 +0800

    删除electron里面的请求方法

--- src/components/zh-request/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    useAxios: true,
-    useIpcRender: false,
-};
--- src/components/zh-request/index.ts
+++ src/components/zh-request/index.ts
@@ -1,46 +1,20 @@
-import IpcRenderRequest from './ipcRenderRequest/ipcRendererRequest';
 import AxiosRequest from './axios/axiosRequest';
-import Config from './config';
 import { TZHRequestParams } from './type';
 
 export default class ZHRequest {
-  useAxios: boolean;
-  instance?: AxiosRequest | IpcRenderRequest;
-  useIpcRender: boolean;
+  instance?: AxiosRequest;
   constructor() {
-    this.useAxios = Config.useAxios;
-    this.useIpcRender = Config.useIpcRender;
-
-    if (this.useAxios) {
-      this.instance = new AxiosRequest();
-    } else if (this.useIpcRender) {
-      this.instance = new IpcRenderRequest();
-    }
+    this.instance = new AxiosRequest();
   }
 
   static async get(params: TZHRequestParams) {
-    const useAxios = Config.useAxios;
-    const useIpcRender = Config.useIpcRender;
-    if (useAxios) {
-      const request = new AxiosRequest();
-      return await request.get(params);
-    } else if (useIpcRender) {
-      const request = new IpcRenderRequest();
-      // return await request.get(params);
-    }
+    const request = new AxiosRequest();
+    return await request.get(params);
   }
 
   static async post(params: TZHRequestParams) {
-    const useAxios = Config.useAxios;
-    const useIpcRender = Config.useIpcRender;
-    if (useAxios) {
-      const request = new AxiosRequest();
-      // console.log('请求参数: ', params);
-      return await request.post(params);
-    } else if (useIpcRender) {
-      const request = new IpcRenderRequest();
-      // return await request.post(params);
-    }
+    const request = new AxiosRequest();
+    return await request.post(params);
   }
 
   static put() {
