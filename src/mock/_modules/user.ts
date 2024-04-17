a5650de8deb533f2d77da039317e5826a1be8572
zzh <956159241@qq.com>
2024-04-17 23:44:30 +0800

    优化下拉选择

--- src/mock/_modules/user.ts
+++ src/mock/_modules/user.ts
@@ -4,6 +4,36 @@ import Mock from 'mockjs';
 export default [
   // GetUserList
   {
+    url: '/apiMock/user/list1',
+    type: 'post',
+    response: (conditions: any) => {
+      const result = {
+        success: true,
+        errorCode: '',
+        errorMessage: '',
+        exceptionMessage: '',
+        data: {
+          total: 1000,
+        },
+      };
+
+      result.data[`records`] = [
+        {
+          id: 1, // 加一
+          name: '@cname', // 随机姓名
+          'sex|0-1': 0, // 0-1随机
+          'status|0-1': 0, // 0-1随机
+          'age|0-180': 180, // 0-180随机
+          phone: '@phone',
+          employeeNum: '@workNumber',
+          'role|1': ['产品', '开发', '测试', '运营'], //数组中的一个，随机的
+        }
+      ];
+
+      return result;
+    },
+  },
+  {
     url: '/apiMock/user/list',
     type: 'post',
     response: (conditions: any) => {
