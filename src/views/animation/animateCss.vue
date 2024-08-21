ba19d90b51944bafc5f9b89ba068e147e4b86a7d
zhangzhonghua <956159241@qq.com>
2024-08-21 22:46:53 +0800

    添加animatecss示例

---
+++ src/views/animation/animateCss.vue
@@ -0,0 +1,51 @@
+<template>
+    <Box :use-scroll-content-box="true" class="scroll-box">
+        <div class="box">
+            <div class="animate__animated animate__bounce animate__infinite">
+                <div
+                    style="width: 200px; height: 200px; border-radius: 200px; background: linear-gradient(red, green, transparent);">
+                </div>
+            </div>
+
+            <div class="animate__animated animate__fadeInLeft animate__infinite">
+                <div
+                    style="width: 200px; height: 200px; border-radius: 200px; background:radial-gradient(yellow, green, transparent);">
+                </div>
+            </div>
+
+            <div class="animate__animated animate__pulse animate__infinite">
+                <div
+                    style="width: 200px; height: 200px; border-radius: 200px; background:radial-gradient(white, green, transparent);">
+                </div>
+            </div>
+        </div>
+
+
+
+    </Box>
+</template>
+
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import Box from '@/components/zh-box/index.vue';
+import 'animate.css';
+
+
+</script>
+
+<style lang="scss" scoped>
+.scroll-box {
+    background-image: url(https://michalsnik.github.io/aos/img/bg.jpg);
+    backdrop-filter: blur(1px);
+}
+
+.box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 100px;
+}
+</style>
