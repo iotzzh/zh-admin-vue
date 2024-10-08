789d9bf795bb426e77c26220b54daa7267bdfed8
zhangzhonghua <956159241@qq.com>
2024-10-08 23:15:15 +0800

    update code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordNormalized } from 'vue-router';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import { close, start } from '@/utils/nporgress';
 import Layout from '@/layout/index.vue';
 import routerData from './routes/index';
@@ -10,7 +10,8 @@ const PUBLIC_PATH = import.meta.env.VITE_PUBLIC_PATH;
 const ROUTE_DATA_SOURCE = import.meta.env.VITE_ROUTE_DATA_SOURCE || 'file';
 
 const router = createRouter({
-  history: createWebHistory(PUBLIC_PATH),
+  // history: createWebHistory(PUBLIC_PATH),
+  history: createWebHashHistory(PUBLIC_PATH),
   routes: [
     {
       path: '/',
