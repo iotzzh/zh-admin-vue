47d03240b86b3be091cca399e13dc142285024bc
全栈道 <956159241@qq.com>
2025-05-03 23:38:29 +0800

    路由优化：删除无用的方法

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,15 +1,14 @@
 const modules = import.meta.glob('./*.json');
-const ENV = import.meta.env;
 
 const route: Array<any> = [];
 
 for (const path in modules) {
-    if (Object.prototype.hasOwnProperty.call(modules, path)) {
-        const module: any = await modules[path]();
+  if (Object.prototype.hasOwnProperty.call(modules, path)) {
+    const module: any = await modules[path]();
 
-        // TODO: Validate repeat
-        route.push(JSON.parse(JSON.stringify(module)));
-    }
+    // TODO: Validate repeat
+    route.push(JSON.parse(JSON.stringify(module)));
+  }
 }
 
 export default route;
