3405f4a7c8ddbca3c2bbd1eadd496bffac2344aa
全栈道 <956159241@qq.com>
2025-05-06 23:33:51 +0800

    Bugfix, util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ for (const path in modules) {
     if (Object.prototype.hasOwnProperty.call(modules, path)) {
         if (path.indexOf('Helper') === -1) continue;
         const module: any = await modules[path]();
-        if (module.default.classname) {
+        if (module.default.className) {
             utils[module.default.className] = module.default;
         }
     }
